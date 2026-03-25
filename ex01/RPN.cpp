a765708be8dc03933796166def8d92ee7e1a37f6
neoo-n <dana.vauthey@gmail.com>
2026-03-25 14:17:26 +0100

    all cleaned

--- ex01/RPN.cpp
+++ ex01/RPN.cpp
@@ -33,12 +33,12 @@ void	RPN::_parsing(std::string list_input)
 	std::string			temp;
 
 	if (list_input.find_first_not_of("0123456789/*-+ ") != std::string::npos)
-		throw std::invalid_argument("Error");
+		throw std::invalid_argument("Error : invalid argument");
 	while (ss1 >> temp)
 	{
 		if (temp.size() > 1 && (temp.find_first_of("+-*/") != std::string::npos))
 		{
-			throw std::invalid_argument("Error");
+			throw std::invalid_argument("Error : invalid argument");
 		}
 	}
 }
@@ -49,7 +49,9 @@ void	RPN::computing(std::string list_input)
 {
 	std::stringstream	ss1(list_input);
 	std::string			temp;
-	int					value;
+	double				value1;
+	double				value2;
+	double				res;
 
 	this->_parsing(list_input);
 	while (ss1 >> temp)
@@ -58,47 +60,67 @@ void	RPN::computing(std::string list_input)
 		if (temp == "+")
 		{
 			if (this->_value_stack.size() < 2)
-				throw std::length_error("Error");
-			value = this->_value_stack.top();
+				throw std::length_error("Error : not enough number for operation");
+			value1 = this->_value_stack.top();
 			this->_value_stack.pop();
-			this->_value_stack.top() += value;
+			value2 = this->_value_stack.top();
+			this->_value_stack.pop();
+			res = value2 + value1;
+			if (res > 2147483647)
+				throw std::overflow_error("Error : overflow");
+			this->_value_stack.push(res);
 		}
 		else if (temp == "-")
 		{
 			if (this->_value_stack.size() < 2)
-				throw std::length_error("Error");
-			value = this->_value_stack.top();
+				throw std::length_error("Error : not enough number for operation");
+			value1 = this->_value_stack.top();
 			this->_value_stack.pop();
-			this->_value_stack.top() -= value;
+			value2 = this->_value_stack.top();
+			this->_value_stack.pop();
+			res = value2 - value1;
+			if (res > 2147483647)
+				throw std::overflow_error("Error : overflow");
+			this->_value_stack.push(res);
 		}
 		else if (temp == "/")
 		{
 			if (this->_value_stack.size() < 2)
-				throw std::length_error("Error");
-			value = this->_value_stack.top();
+				throw std::length_error("Error : not enough number for operation");
+			value1 = this->_value_stack.top();
 			this->_value_stack.pop();
-			if (value == 0)
+			value2 = this->_value_stack.top();
+			this->_value_stack.pop();
+			if (value1 == 0.0)
 				throw std::logic_error("Error");
-			this->_value_stack.top() /= value;
+			res = value2 / value1;
+			if (res > 2147483647)
+				throw std::overflow_error("Error : overflow");
+			this->_value_stack.push(res);
 		}
 		else if (temp == "*")
 		{
 			if (this->_value_stack.size() < 2)
-				throw std::length_error("Error");
-			value = this->_value_stack.top();
+				throw std::length_error("Error : not enough number for operation");
+			value1 = this->_value_stack.top();
 			this->_value_stack.pop();
-			this->_value_stack.top() *= value;
+			value2 = this->_value_stack.top();
+			this->_value_stack.pop();
+			res = value2 * value1;
+			if (res > 2147483647)
+				throw std::overflow_error("Error : overflow");
+			this->_value_stack.push(res);
 		}
 		else
 		{
 			ss << temp;
-			ss >> value;
-			if (value > 9 || value < 0)
-				throw std::invalid_argument("Error");
-			this->_value_stack.push(value);
+			ss >> value1;
+			if (value1 > 9 || value1 < 0)
+				throw std::invalid_argument("Error : number not between 0 and 9");
+			this->_value_stack.push(value1);
 		}
 	}
 	if (this->_value_stack.size() != 1)
-		throw std::invalid_argument("Error");
+		throw std::invalid_argument("Error : not enough operation");
 	std::cout << this->_value_stack.top() << std::endl;
 }
